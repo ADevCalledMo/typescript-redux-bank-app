bf5ee164b2b457a71eaee8a842b1e967d8ea4ccb
ADevCalledMo <mozeswalker@outlook.com>
2022-07-21 15:06:32 +0100

    added store + refactored

---
+++ src/state/store.ts
@@ -0,0 +1,9 @@
+import { legacy_createStore, applyMiddleware  } from "redux";
+import reducers from "./reducers/index";
+import thunk from "redux-thunk";
+
+export const store = legacy_createStore(
+    reducers, 
+    {},
+    applyMiddleware(thunk)
+)
